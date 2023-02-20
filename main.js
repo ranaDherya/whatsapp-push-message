7f774c7693d5f572aa8279be17b987dd8c348a79
ranaDherya <ranaDherya09@gmail.com>
2023-02-20 23:20:55 +0530

    created the js script to send whatsapp message

---
+++ main.js
@@ -0,0 +1,13 @@
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const from = process.env.FROM_WHATSAPP_NO;
+const to = process.env.TO_WHATSAPP_NO;
+const client = require("twilio")(accountSid, authToken);
+
+client.messages
+  .create({
+    from: `whatsapp:${from}`,
+    body: "A new commit to your github repo",
+    to: `whatsapp:${to}`,
+  })
+  .then((message) => console.log(message.sid));
