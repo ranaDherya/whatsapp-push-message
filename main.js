6af9ce0c7e72cecc250433267e81be10bd4bda73
Dherya Rana <ranaDherya09@gmail.com>
2023-02-21 00:23:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const accountSid = "ACabc7cda8ea71e834d0c7434284c3256b";
-const authToken = "c94483638b0facde46f03b3cb3c3e5a7";
+const accountSid = process.env.account_sid;
+const authToken = process.env.auth_token;
 const from = process.env.from_whatsapp_no;
 const to = process.env.to_whatsapp_no;
 const client = require("twilio")(accountSid, authToken);
