5b7f0d2a60e9bb41c2d978ecdee8e847e30df977
Dherya Rana <ranaDherya09@gmail.com>
2023-02-21 00:54:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ client.messages
     to: `whatsapp:${to}`,
   })
   .then((message) => console.log(message.sid));
+
+// const accountSid = 'ACabc7cda8ea71e834d0c7434284c3256b'; 
+// const authToken = '[Redacted]'; 
+// const client = require('twilio')(accountSid, authToken); 
+ 
+// client.messages 
+//       .create({ 
+//          body: 'Your appointment is coming up on July 21 at 3PM', 
+//          from: 'whatsapp:+14155238886',       
+//          to: 'whatsapp:+919926073186' 
+//        }) 
+//       .then(message => console.log(message.sid)) 
+//       .done();
